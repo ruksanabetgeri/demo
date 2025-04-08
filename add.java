92bb2d31a43b35e5f9702abe3f39fb5c5425a59f
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-08 12:21:39 +0530

    save

--- add.java
+++ add.java
@@ -5,9 +5,11 @@ public class add {
         int a=5;
         int b=6;
         int sub=a-b;
+        int sum=a+b;
+       
 
         System.out.println("substraction of a and b is :"+sub);
-        
+        System.out.println("addition of a and b is:"+sum);
     }
 
 
