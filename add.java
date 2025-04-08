12eb1fae8067932afa7efd6d0a54b39838d3fd4c
ruksanabetgeri <ruksanabetageri300@gmail.com>
2025-04-08 11:49:26 +0530

    save

---
+++ add.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class add {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int sum=a+b;
+
+        System.out.println("addition of a and b is :"+sum);
+        
+    }
+
+
+}
